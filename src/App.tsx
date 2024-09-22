845539c3395918834b91b871037ed138a851f85e
Talha Khalid <hafiz.talhakh@gmail.com>
2024-09-22 14:19:48 +0500

    React version upgraded to 18 and TypeScript configured for better programming experience

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Getting Started</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore officia
+        aliquid excepturi vel explicabo impedit, doloremque exercitationem
+        reiciendis quod! Laboriosam magnam ab ipsam! Earum sapiente facere
+        deleniti corporis ea deserunt.
+      </p>
+    </div>
+  );
+}
+
+export default App;
