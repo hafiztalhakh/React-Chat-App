ac447cf1e5eb63ede79d254b1a22b90def772055
Talha Khalid <hafiz.talhakh@gmail.com>
2024-09-25 13:03:14 +0500

    Emoji Picker added in chat input box

--- src/components/chat/chat_input_box/index.tsx
+++ src/components/chat/chat_input_box/index.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import EmojiPicker, { EmojiClickData } from 'emoji-picker-react';
+import { useState } from 'react';
 
 const ChatInputBox = () => {
+  const [value, setValue] = useState('');
+  const [showPicker, setShowPicker] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleEmojiClick = (data: EmojiClickData) => {
+    setValue((prev) => prev + data.emoji);
+    setShowPicker(false);
+  };
+
+  const toggle = () => {
+    setShowPicker((prev) => !prev);
+  };
+
   return (
     <div className='chatInputBox'>
       <ul className='actionButtonsList'>
@@ -21,12 +38,22 @@ const ChatInputBox = () => {
         </li>
       </ul>
       <div className='messageBox'>
-        <input type='text' placeholder='Type your message here..' />
+        <input
+          type='text'
+          placeholder='Type your message here..'
+          value={value}
+          onChange={handleChange}
+        />
       </div>
       <div className='emojiButton'>
-        <button className='iconButton'>
+        <button className='iconButton' onClick={toggle}>
           <img className='icon' src='/assets/emoji.png' alt='EmojiIcon' />
         </button>
+        {showPicker && (
+          <div className='emojiPicker'>
+            <EmojiPicker onEmojiClick={handleEmojiClick} />
+          </div>
+        )}
       </div>
       <div className='sendButton'>
         <button>Send</button>
