80177bb5b70b1fc70f2f7a7ef9f0842e13e7b915
Talha Khalid <hafiz.talhakh@gmail.com>
2024-09-25 17:41:57 +0500

    working on Chat Info

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,83 @@ function App() {
         <ChatBox />
         <ChatInputBox />
       </div>
-      <div className='chatInfoContainer'>Chat Info Container</div>
+      <div className='chatInfoContainer'>
+        <header>
+          <div className='avatar'>
+            <img src='/assets/avatar.png' alt='ChatInfoUserAvatar' />
+          </div>
+          <h2>Ammar</h2>
+          <p className='description'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
+        </header>
+        <div className='accordion'>
+          <div className='accordionItem'>
+            <h2>
+              <button>
+                <span>Chat Settings</span>
+              </button>
+            </h2>
+          </div>
+          <div className='accordionItem'>
+            <h2>
+              <button>
+                <span>Privacy & help</span>
+              </button>
+            </h2>
+          </div>
+          <div className='accordionItem'>
+            <h2>
+              <button className='active'>
+                <span>Shared Photos</span>
+              </button>
+            </h2>
+            <div className='accordionBody'>
+              <ul className='photosList'>
+                <li>
+                  <img
+                    src='https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load'
+                    alt='photoImage'
+                  />
+                  <span>photo_2024_2.png</span>
+                  <button className='iconButton'>
+                    <img
+                      className='icon'
+                      src='/assets/download.png'
+                      alt='DownloadIcon'
+                    />
+                  </button>
+                </li>
+              </ul>
+              {/* <div className='photos'>
+                <div className='photoItem'>
+                  <div className='photoDetails'>
+                    <img
+                      src='https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load'
+                      alt='photoImage'
+                    />
+                    <span>photo_2024_2.png</span>
+                  </div>
+                  <button className='IconButton'>
+                    <img
+                      className='icon'
+                      src='/assets/download.png'
+                      alt='DownloadIcon'
+                    />
+                  </button>
+                </div>
+              </div> */}
+            </div>
+          </div>
+          <div className='accordionItem'>
+            <h2>
+              <button>
+                <span>Shared Files</span>
+              </button>
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
