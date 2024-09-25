1b046536bd6424f42506968508ad41efbb82295a
Talha Khalid <hafiz.talhakh@gmail.com>
2024-09-25 15:00:34 +0500

    chat box static UI done

--- src/components/chat/chat_box/index.tsx
+++ src/components/chat/chat_box/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ChatBox = () => {
   return (
     <div className='chatBox'>
@@ -8,17 +6,63 @@ const ChatBox = () => {
           <img src='/assets/avatar.png' alt='SenderAvatar' />
         </div>
         <div className='message'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-          <div className='time'>
-            <span>5:00 pm</span>
+          <div className='text'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <div className='right-corner'>
+              <span>5:00 pm</span>
+            </div>
           </div>
         </div>
       </div>
       <div className='messageBubble own'>
         <div className='message'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-          <div className='time'>
-            <span>5:00 pm</span>
+          <div className='text'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <div className='right-corner'>
+              <span>5:00 pm</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='messageBubble'>
+        <div className='avatar'>
+          <img src='/assets/avatar.png' alt='SenderAvatar' />
+        </div>
+        <div className='message'>
+          <div className='text'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <div className='right-corner'>
+              <span>5:00 pm</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='messageBubble'>
+        <div className='avatar'>
+          <img src='/assets/avatar.png' alt='SenderAvatar' />
+        </div>
+        <div className='message'>
+          <div className='media'>
+            <img
+              src='https://weimaracademy.org/wp-content/uploads/2021/08/dummy-user.png'
+              alt='DummyImage'
+            />
+          </div>
+          <div className='text'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <div className='right-corner'>
+              <span>5:00 pm</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='messageBubble own'>
+        <div className='message'>
+          <div className='text'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <div className='right-corner'>
+              <span>5:00 pm</span>
+            </div>
           </div>
         </div>
       </div>
